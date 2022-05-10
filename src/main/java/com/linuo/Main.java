605ba36e0e52c7f3ca99612031a712b018ffb6c1
yanruo <123@qq.com>
2022-05-10 19:55:44 +0800

    second commit

--- src/main/java/com/linuo/Main.java
+++ src/main/java/com/linuo/Main.java
@@ -3,5 +3,9 @@ package com.linuo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("my name is linuo");
+        System.out.println("my name is 1");
+        System.out.println("my name is 2");
+        System.out.println("my name is 3");
+        System.out.println("my name is 4");
     }
 }
